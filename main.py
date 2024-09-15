b2f1b3ab08f64cdc411f8930974c5c2a9e57b68e
sivajeetsabdakar <133634489+sivajeetsabdakar@users.noreply.github.com>
2024-09-15 05:42:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def get_lemmebuild_response(input):
 
 st.set_page_config(page_title="Smart Buddy")
 
-st.header("I'm your study buddy, the smarter one")
+st.header("I'm your study buddy, the smarter one �")
 
 if 'history' not in st.session_state:
     st.session_state['history'] = []
